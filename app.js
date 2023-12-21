e5859d3b50e39cc2f4e1dc4aab6a2789c50d370e
trus2lun <53042040+trus2lun@users.noreply.github.com>
2023-12-21 21:51:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // PostgreSQL connection configuration
 const pool = new Pool({
